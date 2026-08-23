bc8688b5a9ea531408e70cd33b9f5e55f2c574cd
Karthik Godela <karthikgodela@gmail.com>
2026-08-23 22:25:48 +0530

    Fix API_BASE_URL to relative /api path for Render production deployment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
-export const UPLOADS_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = '/api';
+export const UPLOADS_BASE_URL = '';
 
 export const DAMAGE_TYPES = [
   'Pothole',
